146c9e2519aeb002e59603f89c3053e79a3563f7
Naitik Shrestha <shresthanaitik010@gmail.com>
2026-03-04 21:44:51 +0545

    Done some question of string and numbers also using simple function to get knowledge of function before i start function.

--- workingWithString&Numbers.cpp
+++ workingWithString&Numbers.cpp
@@ -1,3 +1,4 @@
+/*
 
 // Write a function that takes two strings as parameters and returns their  (i.e., join them together). Test this function with two sample strings.
 
@@ -20,7 +21,49 @@ int main()
 
     string result = concatenateStrings(first, second); // here I have call the function to concatenate.
 
-    cout << result << endl; //here simply i have called the function
+    cout << result << endl; //here simply i have print the output
+
+    return 0;
+}
+
+*/
+
+
+/*Done wrong becasue question have asked to use peoper function. (But this is also not in correct)
+
+//Write a function that takes a string and returns its length (number of characters). Use a sample string to demonstrate the function.
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    string length = "Hello World, My Name is Luffy.";
+
+    cout << "The numbers of characters is " << length.length() <<endl;
+
+    return 0;
+}
+*/
+
+
+
+//Write a function that takes a string and returns its length (number of characters). Use a sample string to demonstrate the function.
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int stringslength(string count) //here i have code the function of count the letter.
+{
+    return count.length();
+}
+int main()
+{
+    string text = "Hello World, My Name is Luffy.";
+
+    int result = stringslength(text); //here i have call function to count the letter on string text. 
+
+    cout<< "There is " << result << " number of character in the text string." <<endl; // here i have simply code to print the output.
 
     return 0;
 }
