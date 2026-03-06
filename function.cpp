c39742f511b5422bd77b2114b505daf697255ddc
Naitik Shrestha <shresthanaitik010@gmail.com>
2026-03-06 22:25:04 +0545

    Learning Function

--- function.cpp
+++ function.cpp
@@ -1,3 +1,5 @@
+/*
+
 #include<iostream>
 using namespace std;
 
@@ -10,6 +12,24 @@ int main()
 {
     
     first();
+
+    return 0;
+}
+
+*/
+
+
+#include<iostream>
+using namespace std;
+
+void second(string name, int age, float height)
+{
+    cout << "Hello your name is " <<name <<". Your age is " <<age <<". Your height is " <<height;
+}
+
+int main()
+{
+    second("Luffy", 17, 5.6);
 
     return 0;
 }
