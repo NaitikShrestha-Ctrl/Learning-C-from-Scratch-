c82cf47b0d87fae1f3e80583e6ed290a11074d8f
Naitik Shrestha <shresthanaitik010@gmail.com>
2026-03-17 20:45:01 +0545

    Add C++ program to print numbers from 1 to 100 using a for loop

--- loops.cpp
+++ loops.cpp
@@ -1,3 +1,5 @@
+/*
+
 #include<iostream>
 using namespace std;
 int main()
@@ -9,3 +11,21 @@ int main()
     
     return 0;
 }
+
+*/
+
+
+// Write a C++ program using a for loop to print numbers from 1 to 100.
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int a = 1; a <= 100; a++)
+    {
+        cout << a << endl;
+    }
+    
+    return 0;
+}
+
